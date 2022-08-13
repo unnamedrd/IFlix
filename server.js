694530a94e679092cdcfd9b3c54e4b32cfcbd9c4
najwanan <theunnamedrd@gmail.com>
2022-08-13 13:54:51 -0700

    adding middleware

--- server.js
+++ server.js
@@ -11,3 +11,27 @@ const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
 const PORT = 8100;
+
+let db, 
+    dbConnectionStr = process.env.DB_STRING, 
+    dbName = "sample_mflix", 
+    collection
+
+//get statement to connect to port 
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(process.env.PORT || PORT, () => {
+        console.log("The port is running ${PORT}, you betta go catch it")
+    })
